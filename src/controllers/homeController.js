d9165215a91aa322b4fd7429fd23ded59e06997f
rogervalentim <valentim.roger33@gmail.com>
2022-10-04 23:11:53 -0300

    finalizando o design da home

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,27 @@
+const products = [
+  {
+    id: 1,
+    name: "Língua de Gato Tablete ao Leite 100G",
+    price: "R$ 26,90",
+    image: "/images/products/kopenhagen-produto-1.png",
+  },
+  {
+    id:2,
+    name:"Língua de Gato Duas Unidades 170G",
+    price: "R$56,90",
+    image:"/images/products/kopenhagen-produto-2.png"
+  },
+  {
+    id:3,
+    name:"Língua de Gato Duas Unidades 170G",
+    price: "R$59,90",
+    image:"/images/products/kopenhagen-produto-3.png"
+  }
+];
+
 const homeController = {
   index: (req, res) => {
-    return res.render("home");
+    return res.render("home", { products: products });
   },
 };
 
